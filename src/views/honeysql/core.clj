a475d0e5c36d7ced1e43ce9300e8ff64a0b831f8
gered <gered@blarg.ca>
2016-05-20 18:35:55 -0400

    update function doc comments

--- src/views/honeysql/core.clj
+++ src/views/honeysql/core.clj
@@ -13,7 +13,7 @@
   ((:put-hints-fn @view-system) hints))
 
 (defmacro with-view-transaction
-  "Like with-db-transaction, but sends view hints at end of transaction."
+  "Like jdbc's with-db-transaction, but sends view hints at end of transaction."
   [binding & forms]
   (let [tvar (first binding), db (second binding), args (drop 2 binding)]
     `(if (:hints ~db) ;; check if we are in a nested transaction
@@ -25,7 +25,7 @@
          (send-hints! @hints#)
          result#))))
 
-(defn execute-honeysql!
+(defn- execute-honeysql!
   "Always return keys for inserts."
   [db hsql-map]
   (if-let [table (:insert-into hsql-map)]
@@ -38,7 +38,8 @@
   "Used to perform arbitrary insert/update/delete actions on the database,
    while ensuring that view hints are sent to the view system.
    Arguments are:
-   - db: a clojure.java.jdbc database with fid field
+   - db: a clojure.java.jdbc database connection
+   - namespace (optional): a namespace that will be included in the hints sent out
    - action-map: the HoneySQL map for the insert/update/delete action"
   ([db namespace action-map]
    (let [results   (execute-honeysql! db action-map)
